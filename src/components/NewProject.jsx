2d7938d4e2cc7954379cf0cd00b9df6e3665e2b0
Kimhanjin <ddang0103@naver.com>
2024-10-04 21:54:40 +0900

    Feature: 입력 인풋들에 대한 Validation체크 및 모달창 띄우기
     - useImperative 를 통한 Dom 에 접근하여 내장된 showModal로 모달창 띄움

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,28 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({handleAddProject}) {
-    const  title = useRef();
-    const  desctription = useRef();
-    const  dueDate = useRef();
+export default function NewProject({ handleAddProject }) {
+
+    const modal = useRef();
+
+    const title = useRef();
+    const desctription = useRef();
+    const dueDate = useRef();
 
     function handleSave() {
         const ennteredTitle = title.current.value;
         const enteredDescription = desctription.current.value
         const enteredDueDate = dueDate.current.value
-    
+
+        if ((!ennteredTitle || ennteredTitle.trim() === '') ||
+            (!enteredDescription || enteredDescription.trim() === '') ||
+            (!enteredDueDate || enteredDueDate.trim() === '')) {
+                modal.current.open()
+                return
+        }
+
+
         handleAddProject({
             title: ennteredTitle,
             desctription: enteredDescription,
@@ -19,25 +31,31 @@ export default function NewProject({handleAddProject}) {
     }
 
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex item-center justify-end gap-4 my-4">
-                <li>
-                    <button className="px-6 py-2 text-stone-800  hover:text-stone-950">
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50  hover:bg-stone-950"
-                    onClick={handleSave}>
-                        Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-               <Input type="text" ref={title} label="Title"/>
-               <Input ref={desctription} label="Desctription" textarea/>
-               <Input type="date" ref={dueDate} label="Due Date"/>
+        <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2> Invalid Input </h2>
+            <p>enter right Value</p>
+        </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex item-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="px-6 py-2 text-stone-800  hover:text-stone-950">
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50  hover:bg-stone-950"
+                            onClick={handleSave}>
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title" />
+                    <Input ref={desctription} label="Desctription" textarea />
+                    <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
